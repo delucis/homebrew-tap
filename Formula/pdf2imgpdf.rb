5882e283c8ac86da75db864cefa2a86a8f99a8f5
delucis <swithinbank@gmail.com>
2017-08-16 18:15:13 +0200

    Bump pdf2imgpdf version to 1.1.0

--- Formula/pdf2imgpdf.rb
+++ Formula/pdf2imgpdf.rb
@@ -1,9 +1,9 @@
 class Pdf2imgpdf < Formula
   desc "A command line tool to convert a PDF to a PDF of PNG images."
   homepage "https://github.com/delucis/pdf2imgpdf"
-  url "https://github.com/delucis/pdf2imgpdf/archive/1.0.0.tar.gz"
-  sha256 "e248eddbeacd35245bc5990651211f1c4546535bc58fd02df18c80f3c41966e6"
-  version "1.0.0"
+  url "https://github.com/delucis/pdf2imgpdf/archive/1.1.0.tar.gz"
+  sha256 "663068c42619ec0e4f672acebfe28087e449b2bc9ee522418af6adce6f133856"
+  version "1.1.0"
 
   depends_on "poppler"
   depends_on "imagemagick"
